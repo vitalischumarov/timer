4eea86bf80bf63abc7647eda91a5883697202f29
Vitali Schumarov <v.schumarov@icloud.com>
2025-03-24 20:52:50 +0100

    works

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.scss";
 import Input from "./components/Input";
 import Button from "./components/Button";
@@ -6,29 +6,61 @@ import TimerLeft from "./components/TimerLeft";
 import "./App.scss";
 
 function App() {
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState<string>("");
+  const [timeLeft, setTimeLeft] = useState<number>(Number(input));
+  const [timerIsRunning, setTimerIsRunning] = useState<boolean>(false);
+  const idRef = useRef<number>();
 
   function handleInput(event: React.ChangeEvent<HTMLInputElement>) {
     const newInput: string = event.target.value;
     setInput(newInput);
+    setTimeLeft(Number(newInput));
+  }
+
+  function startTimer() {
+    setTimerIsRunning(true);
+    console.log("start");
+  }
+
+  useEffect(() => {
+    if (timerIsRunning) {
+      idRef.current = setInterval(() => {
+        setTimeLeft((prevTime) => {
+          if (prevTime > 0) {
+            return prevTime - 0.001;
+          } else {
+            clearInterval(idRef.current);
+            setTimerIsRunning(false);
+            return 0;
+          }
+        });
+      }, 10);
+      console.log(idRef.current);
+    } else {
+      clearInterval(idRef.current);
+    }
+    return () => {};
+  }, [timerIsRunning]);
+
+  function pauseTimer() {
+    clearInterval(idRef.current);
+    setTimerIsRunning(false);
   }
 
   function resetTimer() {
-    setInput("0");
+    clearInterval(idRef.current);
+    setTimeLeft(Number(input));
+    setTimerIsRunning(false);
   }
-
-  function startTimer() {}
-
-  function pauseTimer() {}
 
   return (
     <div className="app">
       <h3>Zeit festlegen:</h3>
       <Input onclick={handleInput} timer={input}></Input>
-      <TimerLeft counter={input}></TimerLeft>
+      <TimerLeft counter={timeLeft}></TimerLeft>
       <div className="btn_row">
-        <Button title="Start" onclick={resetTimer}></Button>
-        <Button title="Pause" onclick={resetTimer}></Button>
+        <Button title="Start" onclick={startTimer}></Button>
+        <Button title="Pause" onclick={pauseTimer}></Button>
         <Button title="Reset" onclick={resetTimer}></Button>
       </div>
     </div>
--- src/components/TimerLeft.tsx
+++ src/components/TimerLeft.tsx
@@ -1,18 +1,17 @@
 interface TimerLeftProps {
-  counter: string;
+  counter: number;
 }
 
 function TimerLeft({ counter }: TimerLeftProps) {
   function formatTime(time: number): string {
     let formatTime: string = time.toFixed(3);
-    console.log(formatTime);
     return formatTime;
   }
 
   return (
     <div>
       <h1>{"Time left:"}</h1>
-      <h1>{formatTime(Number(counter))}</h1>
+      <h1>{`${formatTime(counter)} s`}</h1>
     </div>
   );
 }
